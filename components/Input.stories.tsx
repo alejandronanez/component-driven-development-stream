257ea5fa6cc2b9af5e77c6df98f6be3e68f3f5b1
Alejandro Nanez <git@alejandro.dev>
2022-04-17 16:35:44 -0500

    first stories for input

---
+++ components/Input.stories.tsx
@@ -0,0 +1,22 @@
+// Input.stories.ts|tsx
+
+import React from "react";
+
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+import { Input } from "./Input";
+
+export default {
+  title: "Input Component",
+  component: Input,
+} as ComponentMeta<typeof Input>;
+
+export const Disabled: ComponentStory<typeof Input> = () => (
+  <Input disabled={true} />
+);
+
+export const Enabled: ComponentStory<typeof Input> = () => <Input />;
+
+export const WithError: ComponentStory<typeof Input> = () => (
+  <Input error={true} />
+);
--- components/Input.tsx
+++ components/Input.tsx
@@ -1,3 +1,16 @@
-export function Input() {
-  return <input type="text" />;
+type Props = {
+  error?: boolean;
+  disabled?: boolean;
+};
+
+export function Input({ disabled, error }: Props) {
+  return (
+    <input
+      disabled={disabled}
+      type="text"
+      style={{
+        border: error ? "1px solid red" : "1px solid gray",
+      }}
+    />
+  );
 }
